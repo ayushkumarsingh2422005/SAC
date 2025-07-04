d0c0ba422748363de3fd19ff9b4f296dd1a41a3b
Ayush Kumar Singh <ayush2422005@gmail.com>
2025-07-04 16:35:33 +0530

    minor chages

--- app/administration/page.js
+++ app/administration/page.js
@@ -274,7 +274,8 @@ export default function page() {
                                         : 'lg:border-transparent border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                                         }`}
                                 >
-                                    {tabLabels[tab].label}
+                                    
+                                    {tab=="sac_office_staff" ? "Office Staff": tabLabels[tab].label}
                                 </button>
                             ))}
                         </div>
